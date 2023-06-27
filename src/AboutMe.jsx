3b99903f1a4d110364dabec7ae114fe697381165
marianIR <marianraileanu37@gmail.com>
2023-06-27 16:58:43 +0300

    feat: about me update

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -5,11 +5,12 @@ const AboutMe = () => {
         <h2 className="about-me-heading">About me</h2>
         <p className="about-me-paragraph">
           Hi! My name is Marian and I am 20 years old and I live in a village
-          from Iasi, Romania. I like to work in a team, to motivate those around
-          me to achieve a goal. I am always ready to learn new things, to meet
-          new people and to be part of a community where I can evolve. As things
-          of interest, I'm interested in motorsports, books, video games,
-          geopolitics, history, environment protection and human rights.
+          from Iasi, Romania. I love the technology that makes our lives better
+          every day. I like to work in a team, to motivate those around me to
+          achieve a goal. I am always ready to learn new things, to meet new
+          people and to be part of a community where I can evolve. As things of
+          interest, I'm interested in motorsports, books, video games and
+          history.
         </p>
       </article>
       <div className="skills">
